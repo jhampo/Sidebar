d8f06e2d235300091a5428b189fa08ca93873aac
Leyton Manuel Espitia Diaz <leyton_espitia@hotmail.com>
2023-09-21 00:01:27 -0500

    Refactoriza main.js y se corrige el botón Dark Mode

--- design-html/js/main.js
+++ design-html/js/main.js
@@ -7,36 +7,26 @@ const circulo = document.querySelector(".circulo");
 const menu = document.querySelector(".menu");
 const main = document.querySelector("main");
 
-menu.addEventListener("click",()=>{
+menu.addEventListener("click", () => {
     barraLateral.classList.toggle("max-barra-lateral");
-    if(barraLateral.classList.contains("max-barra-lateral")){
-        menu.children[0].style.display = "none";
-        menu.children[1].style.display = "block";
-    }
-    else{
-        menu.children[0].style.display = "block";
-        menu.children[1].style.display = "none";
-    }
-    if(window.innerWidth<=320){
+    const isMaxBarraLateral = barraLateral.classList.contains("max-barra-lateral");
+    menu.children[0].style.display = isMaxBarraLateral ? "none" : "block";
+    menu.children[1].style.display = isMaxBarraLateral ? "block" : "none";
+
+    if (window.innerWidth <= 320) {
         barraLateral.classList.add("mini-barra-lateral");
         main.classList.add("min-main");
-        spans.forEach((span)=>{
-            span.classList.add("oculto");
-        })
+        spans.forEach((span) => span.classList.add("oculto"));
     }
 });
 
-palanca.addEventListener("click",()=>{
-    let body = document.body;
-    body.classList.toggle("dark-mode");
-    body.classList.toggle("");
+palanca.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
     circulo.classList.toggle("prendido");
 });
 
-cloud.addEventListener("click",()=>{
+cloud.addEventListener("click", () => {
     barraLateral.classList.toggle("mini-barra-lateral");
     main.classList.toggle("min-main");
-    spans.forEach((span)=>{
-        span.classList.toggle("oculto");
-    });
+    spans.forEach((span) => span.classList.toggle("oculto"));
 });
